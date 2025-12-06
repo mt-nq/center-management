f433e5a313a7e1ee72eb6faa81df1c58ac532cd3
Viet Anh <anhdv1508.ptit@gmail.com>
2025-12-06 16:14:27 +0700

    thêm get ds progress

--- src/main/java/com/example/center_management/controller/admin/AdminProgressController.java
+++ src/main/java/com/example/center_management/controller/admin/AdminProgressController.java
@@ -1,19 +1,29 @@
 package com.example.center_management.controller.admin;
 
+import java.util.List;
+
 import com.example.center_management.dto.response.EnrollmentProgressResponse;
 import com.example.center_management.service.ProgressService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
 
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class AdminProgressController {
+
     private final ProgressService progressService;
+
     // GET /admin/enrollments/{enrollmentId}/progress  (Admin)
-    @GetMapping("/enrollments/{enrollmentId}/progress")
+    @GetMapping("/admin/enrollments/{enrollmentId}/progress")
     public EnrollmentProgressResponse getProgressForAdmin(@PathVariable Long enrollmentId) {
         return progressService.getProgressForAdmin(enrollmentId);
+    }
+
+    @GetMapping("/admin/enrollments/progress")
+    public List<EnrollmentProgressResponse> getAllProgressForAdmin() {
+        return progressService.getAllEnrollmentProgressForAdmin();
     }
 }
